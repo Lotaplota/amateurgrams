2e30e3f884bf21d3f98133a8a778f43f06e514fd
lotaplota <kristhian.lucas@gmail.com>
2024-10-21 11:04:35 -0300

    implemented some features, but mainly the PopulateItems() method don't think it's complete though

--- biller/biller/Program.cs
+++ biller/biller/Program.cs
@@ -41,7 +41,7 @@ void PopulatePeople()
         
         for (int i = 0; i < people.Count; i++)
         {
-            Console.WriteLine($"{i}. {people[i].Name}");
+            Console.WriteLine($"{i + 1}. {people[i].Name}");
         }
 
         input = GetString("enter name: ");
@@ -53,10 +53,63 @@ void PopulatePeople()
     }
 }
 
+void PopulateItems()
+{
+    while (true)
+    {
+        string input;
+
+        Console.Clear();
+        Console.WriteLine("enter the starting items and their prices (enter 'done' when done)\nif you don't specify anyone, everyone will be involved");
+
+        for ( int i = 0; i < items.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {items[i].Name} {items[i].Value}");
+        }
+
+        input = GetString("enter the item's name, price, and users"); // maybe add a loop that forces the user to input correctly
+        string[] words = input.Split();
+
+        // Breaks the loop if the user enters 'done'
+        if (input == "done") { break; }
+        else
+        {
+            Item newItem = new Item(words[0], Convert.ToSingle(words[1]));
+            items.Add(newItem);
+
+            // If no person is specified, links everyone to the current item
+            if (words.Length == 2)
+            {
+                for (int i = 0; i < people.Count; i++)
+                {
+                    links.Add(new Link(people[i], newItem));
+                }
+            }
+
+            // If more than 2 command line arguments are provided, links the item only to the specified people
+            else if (words.Length > 2)
+            {
+                // Loops starting from the third command line
+                for (int i = 2; i < words.Length; i++)
+                {
+                    links.Add(new Link(GetPerson(words[i]), newItem));
+                }
+            }
+            
+        }
+    }
+}
+
 string GetString(string prompt)
 {
     Console.Write(prompt);
     return Console.ReadLine();
+}
+
+float GetFloat(string prompt)
+{
+    Console.Write(prompt);
+    return Convert.ToSingle(Console.ReadLine());
 }
 
 void DisplayPersonInfo(string name)
@@ -112,6 +165,12 @@ public class Item
 {
     public string? Name {get; private set;}
     public float Value {get; private set;}
+
+    public Item(string name, float value)
+    {
+        Name = name;
+        Value = value;
+    }
 }
 
 public class Link
@@ -123,5 +182,10 @@ public class Link
     {
         Target = target;
         Item = item;
+    }
+
+    public override string ToString()
+    {
+        return $"{Target} bought {Item}";
     }
 }
