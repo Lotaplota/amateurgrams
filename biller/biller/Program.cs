e14c2ff3347733b79e338201ecd8599a110dc071
lotaplota <kristhian.lucas@gmail.com>
2024-10-23 11:46:03 -0300

    implemented ListPeople() method

--- biller/biller/Program.cs
+++ biller/biller/Program.cs
@@ -8,259 +8,269 @@
     {
         PopulatePeople();
         PopulateItems();
+        GetMainInput();
+    }
 
-        // Shows the user a list of available options, then prompts them for an input
-        void GetMainInput()
+    // Shows the user a list of available options, then prompts them for an input
+    void GetMainInput()
+    {
+        Console.WriteLine("What would you like to do? (enter a number)\n" +
+        "1. list people, items, or links" +
+        "2. describe person or items" +
+        "5. update person or item" +
+        "6. describe item" +
+        "7. update person" +
+        "8. update item" +
+        "3. add person" +
+        "4. add item");
+
+        while (true)
         {
-            Console.WriteLine("What would you like to do? (enter a number)\n" +
-            "1. list people, items, or links" +
-            "2. describe person or items" +
-            "5. update person or item" +
-            "6. describe item" +
-            "7. update person" +
-            "8. update item" +
-            "3. add person" +
-            "4. add item");
-            while (true)
+            string input = GetString("Choose an option (number or text)"); // CONTINUE implement these branches
+
+            IBranch command = input switch
             {
-                string input = GetString("Choose an option (number or text)"); // CONTINUE implement these branches
-
-                IBranch command = input switch
-                {
-                    "1" => new ListBranch(),
-                    "2" => new DescribeBranch(),
-                    "3" => new UpdateBranch(),
-                    "4" => new AddBranch(),
-                    _ => new VoidBranch()
-                };
-            }
+                "1" => new ListBranch(),
+                "2" => new DescribeBranch(),
+                "3" => new UpdateBranch(),
+                "4" => new AddBranch(),
+                _ => new VoidBranch()
+            };
         }
+    }
 
-        void PopulatePeople()
+    void PopulatePeople()
+    {
+        string input;
+
+        // Console.Clear(); DONKEY
+
+        input = GetString("enter each user's name: ");
+        string[] words = input.Split();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            Console.WriteLine($"Added {words[i]}");
+            people.Add(new Person(words[i]));
+        }
+    }
+
+    void PopulateItems()
+    {
+        while (true)
         {
             string input;
 
             // Console.Clear(); DONKEY
+            Console.WriteLine("enter the starting items and their prices (enter 'done' when done)\n" +
+            "if you don't specify anyone, everyone will contribute");
 
-            input = GetString("enter each user's name: ");
-            string[] words = input.Split();
-
-            for (int i = 0; i < words.Length; i++)
+            for (int i = 0; i < items.Count; i++)
             {
-                Console.WriteLine($"Added {words[i]}");
-                people.Add(new Person(words[i]));
+                Console.WriteLine($"{i + 1}. {items[i].Name} {items[i].Price}");
             }
-        }
 
-        void PopulateItems()
-        {
-            while (true)
-            {
-                string input;
+            input = GetString("enter the item's name, price, and users: "); // MAYBE add a loop that forces the user to input correctly
 
-                // Console.Clear(); DONKEY
-                Console.WriteLine("enter the starting items and their prices (enter 'done' when done)\n" +
-                "if you don't specify anyone, everyone will contribute");
-
-                for (int i = 0; i < items.Count; i++)
-                {
-                    Console.WriteLine($"{i + 1}. {items[i].Name} {items[i].Price}");
-                }
-
-                input = GetString("enter the item's name, price, and users: "); // MAYBE add a loop that forces the user to input correctly
-
-                // Breaks the loop if the user enters 'done'
-                if (input == "done") { break; }
-                else
-                {
-                    // Splitting the input into an array of words to adress possible interpretations
-                    string[] words = input.Split();
-
-                    // Creates a new item with the current information and adds it to the list of items
-                    Item newItem = new(words[0], Convert.ToSingle(words[1]));
-                    items.Add(newItem);
-
-                    // If no person is specified, links everyone to the current item
-                    if (words.Length == 2)
-                    {
-                        for (int i = 0; i < people.Count; i++)
-                        {
-                            links.Add(new(people[i], newItem));
-                        }
-                    }
-
-                    // If more than 2 command line arguments are provided, links the item only to the specified people
-                    else if (words.Length > 2)
-                    {
-                        // Loops starting from the third command line argument
-                        for (int i = 2; i < words.Length; i++)
-                        {
-                            links.Add(new(GetPerson(words[i]), newItem));
-                        }
-                    }
-                }
-            }
-        }
-
-        // Adds a new item to the list of items
-        // Links that item to everyone on the list if nobody else is specified
-        void AddItem()
-        {
-            // Prompts for the new item name and price, then separates the input into an array
-            string? input = GetString("name the item and its price\n" +
-            "if you don't specify anyone, everyone will contribute\n");
-            string[] words = input.Split();
-
-            // Creates a new item using the given information and adds it to the list
-            Item newItem = new(words[0], Convert.ToSingle(words[1]));
-            items.Add(newItem);
-
-            // If 2 arguments are given, links the item to everyone
-            if (words.Length == 2)
-            {
-                for (int i = 0; i < people.Count; i++)
-                {
-                    links.Add(new(people[i], newItem));
-                }
-            }
-            // If more than 2 command line arguments are given, links the item to the specified people
-            // starting from the 3rd command line argument
+            // Breaks the loop if the user enters 'done'
+            if (input == "done") { break; }
             else
             {
-                for (int i = 2; i < words.Length; i++)
+                // Splitting the input into an array of words to adress possible interpretations
+                string[] words = input.Split();
+
+                // Creates a new item with the current information and adds it to the list of items
+                Item newItem = new(words[0], Convert.ToSingle(words[1]));
+                items.Add(newItem);
+
+                // If no person is specified, links everyone to the current item
+                if (words.Length == 2)
                 {
-                    links.Add(new(GetPerson(words[i]), newItem));
+                    for (int i = 0; i < people.Count; i++)
+                    {
+                        links.Add(new(people[i], newItem));
+                    }
+                }
+
+                // If more than 2 command line arguments are provided, links the item only to the specified people
+                else if (words.Length > 2)
+                {
+                    // Loops starting from the third command line argument
+                    for (int i = 2; i < words.Length; i++)
+                    {
+                        links.Add(new(GetPerson(words[i]), newItem));
+                    }
                 }
             }
         }
+    }
 
-        void AddPerson()
+    // Adds a new item to the list of items
+    // Links that item to everyone on the list if nobody else is specified
+    void AddItem()
+    {
+        // Prompts for the new item name and price, then separates the input into an array
+        string? input = GetString("name the item and its price\n" +
+        "if you don't specify anyone, everyone will contribute\n");
+        string[] words = input.Split();
+
+        // Creates a new item using the given information and adds it to the list
+        Item newItem = new(words[0], Convert.ToSingle(words[1]));
+        items.Add(newItem);
+
+        // If 2 arguments are given, links the item to everyone
+        if (words.Length == 2)
         {
-            // Loops until the user has entered a valid input
-            while (true)
+            for (int i = 0; i < people.Count; i++)
             {
-                string? input = GetString("enter the name of the person: ");
-
-                // Checks if the name is already in the list, if not, creates a person with that name and adds it to the list
-                if (IsPersonDuplicate(input))
-                {
-                    Console.WriteLine("This name is already on the list!");
-                }
-                else
-                {
-                    people.Add(new(input));
-                    break;
-                }
-            } 
+                links.Add(new(people[i], newItem));
+            }
         }
+        // If more than 2 command line arguments are given, links the item to the specified people
+        // starting from the 3rd command line argument
+        else
+        {
+            for (int i = 2; i < words.Length; i++)
+            {
+                links.Add(new(GetPerson(words[i]), newItem));
+            }
+        }
+    }
+
+    void AddPerson()
+    {
+        // Loops until the user has entered a valid input
+        while (true)
+        {
+            string? input = GetString("enter the name of the person: ");
+
+            // Checks if the name is already in the list, if not, creates a person with that name and adds it to the list
+            if (IsPersonDuplicate(input))
+            {
+                Console.WriteLine("This name is already on the list!");
+            }
+            else
+            {
+                people.Add(new(input));
+                break;
+            }
+        } 
+    }
 
         // Calculates the values for a given item taking into account how many candidates are contributing
-        float ShareOf(Item item)
+    float ShareOf(Item item)
+    {
+        int buyers = 0;
+
+        for (int i = 0; i < links.Count; i++)
         {
-            int buyers = 0;
-
-            for (int i = 0; i < links.Count; i++)
+            if (links[i].Item.Name == item.Name)
             {
-                if (links[i].Item.Name == item.Name)
-                {
-                    buyers++;
-                }
-            }
-
-            // Returns the calculated value with two decimal places, rounded up
-            return (float)Math.Round(item.Price / buyers, 2, MidpointRounding.ToPositiveInfinity);
-        }
-
-        void DisplayInfo(Person person)
-        {
-            float debt = 0;
-
-            Console.WriteLine($"{person.Name} bought: ");
-            for (int i = 0; i < links.Count; i++)
-            {
-                if (links[i].Contributor.Name == person.Name)
-                {
-                    Console.WriteLine(links[i].Item.Name + " for " + ShareOf(links[i].Item));
-                    debt += ShareOf(links[i].Item);
-                }
-            }
-            Console.WriteLine("owing the total amount of " + Math.Round(debt, 2, MidpointRounding.ToPositiveInfinity));
-        }
-
-        void DisplayAllInfo()
-        {
-            for (int i = 0; i < people.Count; i++)
-            {
-                Person currentPerson = people[i];
-                List<Item> theirItems = GetItemsFrom(people[i]);
-
-                float owedValue = 0;
-
-                // Sums the value of all items the current person contributes
-                for (int j = 0; j < theirItems.Count; j++)
-                {
-                    owedValue += ShareOf(theirItems[j]);
-                }
-
-                Console.WriteLine($"{currentPerson.Name} owes {owedValue}");
+                buyers++;
             }
         }
 
-        List<Item> GetItemsFrom(Person person)
-        {
-            List<Item> theirItems = [];
+        // Returns the calculated value with two decimal places, rounded up
+        return (float)Math.Round(item.Price / buyers, 2, MidpointRounding.ToPositiveInfinity);
+    }
 
-            for (int i = 0; i < links.Count; i++)
+    void DisplayInfo(Person person)
+    {
+        float debt = 0;
+
+        Console.WriteLine($"{person.Name} bought: ");
+        for (int i = 0; i < links.Count; i++)
+        {
+            if (links[i].Contributor.Name == person.Name)
             {
-                if (links[i].Contributor.Name == person.Name)
-                {
-                    theirItems.Add(links[i].Item);
-                }
+                Console.WriteLine(links[i].Item.Name + " for " + ShareOf(links[i].Item));
+                debt += ShareOf(links[i].Item);
+            }
+        }
+        Console.WriteLine("owing the total amount of " + Math.Round(debt, 2, MidpointRounding.ToPositiveInfinity));
+    }
+
+    void DisplayAllInfo()
+    {
+        for (int i = 0; i < people.Count; i++)
+        {
+            Person currentPerson = people[i];
+            List<Item> theirItems = GetItemsFrom(people[i]);
+
+            float owedValue = 0;
+
+            // Sums the value of all items the current person contributes
+            for (int j = 0; j < theirItems.Count; j++)
+            {
+                owedValue += ShareOf(theirItems[j]);
             }
 
-            return theirItems;
+            Console.WriteLine($"{currentPerson.Name} owes {owedValue}");
+        }
+    }
+
+    List<Item> GetItemsFrom(Person person)
+    {
+        List<Item> theirItems = [];
+
+        for (int i = 0; i < links.Count; i++)
+        {
+            if (links[i].Contributor.Name == person.Name)
+            {
+                theirItems.Add(links[i].Item);
+            }
         }
 
-        // Searches a name in the people list and returns the Person
-        Person GetPerson(string name)
-        {
-            for (int i = 0; i < people.Count; i++)
-            {
-                if (people[i].Name == name)
-                {
-                    return people[i];
-                }
+        return theirItems;
+    }
 
+    // Searches a name in the people list and returns the Person
+    Person GetPerson(string name)
+    {
+        for (int i = 0; i < people.Count; i++)
+        {
+            if (people[i].Name == name)
+            {
+                return people[i];
             }
 
-            // If no person is found, returns a dummy person named 'null'
-            return new("null");
         }
 
-        bool IsPersonDuplicate(string? name)
+        // If no person is found, returns a dummy person named 'null'
+        return new("null");
+    }
+
+    bool IsPersonDuplicate(string? name)
+    {
+        // Loops through the list of people and returns true if the name matches
+        for (int i= 0; i < people.Count; i++)
         {
-            // Loops through the list of people and returns true if the name matches
-            for (int i= 0; i < people.Count; i++)
+            if (people[i].Name == name)
             {
-                if (people[i].Name == name)
-                {
-                    return true;
-                }
+                return true;
             }
-
-            return false;
         }
 
-        string? GetString(string prompt)
-        {
-            Console.Write(prompt);
-            return Console.ReadLine();
-        }
+        return false;
+    }
 
-        float GetFloat(string prompt)
+    string? GetString(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
+
+    float GetFloat(string prompt)
+    {
+        Console.Write(prompt);
+        return Convert.ToSingle(Console.ReadLine());
+    }
+
+    private static void ListPeople()
+    {
+        for (int i = 0; i < people.Count; i++)
         {
-            Console.Write(prompt);
-            return Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"{i + 1}. {people[i].Name}");
         }
     }
 
@@ -282,28 +292,26 @@
 
             while (true)
             {
-                string input = Console.ReadLine();
+                string? input = Console.ReadLine();
 
                 if (input == "1" || input.ToLower() == "people")
                 {
-                    Program.ListPeople();
+                    ListPeople();
+                    break;
                 }
-                else if (input == "2" || string.Equals(input, "items"))
+                else if (input == "2" || input.ToLower() == "items")
                 {
-                    //
+                    ListItems();
+                    break;
                 }
                 else
                 {
-                    //
+                    Console.WriteLine("Invalid input");
                 }
             }
         }
     }
 
-    private static void ListPeople()
-    {
-        // i need access to the list of people contained in the Main() method!!
-    }
 
     public class DescribeBranch : IBranch
     {
